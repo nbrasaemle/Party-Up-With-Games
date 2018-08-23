6563fba6cc26e0356eb3bc9a250764bdbce69a01
nbrasaemle <nicole.brasaemle@gmail.com>
2018-08-23 02:10:21 -0500

    index done, handlebars updates, some js

--- public/js/index.js
+++ public/js/index.js
@@ -86,9 +86,41 @@ function initMap() {
         infowindow.open(map, marker);
     });
 };
+
+$(document).ready(function() {
+    //function to change navbar's buttons (there has to be a key of "signedin" to attach to the navbar-blockers)
+    $(document).on("click", "#sign-in-btn" || "#sign-up-btn", function(){
+
+    });
+    //values from the party form to post
+    $('#new-party').on('click', function (event) {
+      event.preventDefault();
+      console.log('here');
+      var newParty = {
+        gameName: $('#game-name').val().trim(),
+        partyName: $('#party-name').val().trim(),
+        address: $('#address-input').val().trim(),
+        description: $('#description').val().trim(),
+        experience: $('#experience-level').val().trim(),
+        numberOfPlayers: $('#num-players').val().trim(),
+        date: $('#date').val().trim(),
+        time: $('#time').val().trim()
+      };
+      console.log(newParty);
+      $.ajax('/api/parties', {
+        type: 'POST',
+        data: newParty
+      }).then(
+        function () {
+          console.log('posted Party', newParty);
+          window.location.replace('/');
+        }
+      );
+    });
+  });
 // // Get references to page elements
-// var $exampleText = $("#example-text");
-// var $exampleDescription = $("#example-description");
+// var gameName = $("#game-name").val().trim();
+// var partyName = $("#party-name").val().trim();;
 // var $submitBtn = $("#submit");
 // var $exampleList = $("#example-list");
 
