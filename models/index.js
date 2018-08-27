0a197ba03c137789bd79d8e971e50aa17c17b6ea
Andrew Vala <38055092+awvala@users.noreply.github.com>
2018-08-27 18:58:21 -0500

    Fixed image names and added heroku code

--- models/index.js
+++ models/index.js
@@ -6,7 +6,11 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var env = process.env.NODE_ENV || "development";
 var config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+if (config.use_env_variable) {
+    var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+    var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 var db = {};
 
 fs
